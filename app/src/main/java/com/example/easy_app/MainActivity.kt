f815cea2a3c0d1265a6e4610681a0081ab40c300
muscleandhustle <shinobeeee@mail.ru>
2026-03-28 21:00:27 +0700

    Rows_Columns
    Remembering how to do all this things

--- app/src/main/java/com/example/easy_app/MainActivity.kt
+++ app/src/main/java/com/example/easy_app/MainActivity.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.easy_app.ui.theme.Easy_APPTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +44,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    //
+    val clickCount = remember { mutableStateOf(0) }
+
+    Row(modifier = modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Standard Image",
+            modifier = Modifier
+                .size(64.dp)
+                .clickable {
+                    clickCount.value++  // увеличиваем счётчик
+                }
+        )
+        Column(modifier = Modifier.padding(start = 16.dp)) {
+            Text(text = "Hello $name", fontSize = 24.sp)
+            Text(text = "Nice to meet you", fontSize = 16.sp)
+            Text(text = "Clicks: ${clickCount.value}", fontSize = 16.sp)
+        }
+    }
 }
 
 @Preview(showBackground = true)
