5832ee5db1844d14cf4afad9a3cbdd8bf25ab47c
muscleandhustle <shinobeeee@mail.ru>
2026-03-29 08:44:05 +0700

    Change to xml
    Trying during education

--- app/src/main/java/com/example/easy_app/MainActivity.kt
+++ app/src/main/java/com/example/easy_app/MainActivity.kt
@@ -2,74 +2,96 @@ package com.example.easy_app
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.easy_app.ui.theme.Easy_APPTheme
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+//import androidx.activity.ComponentActivity
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+//import androidx.activity.compose.setContent
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.res.painterResource
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import androidx.compose.ui.unit.sp
+//import com.example.easy_app.ui.theme.Easy_APPTheme
+//import androidx.compose.foundation.Image
+//import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.Row
+//import androidx.compose.foundation.layout.size
+//import androidx.compose.runtime.mutableIntStateOf
+//import androidx.compose.runtime.remember
 
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : ComponentActivity() {
+    private var clickCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Easy_APPTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+        setContentView(R.layout.activity_main)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val textClicks = findViewById<TextView>(R.id.textClicks)
+
+        imageView.setOnClickListener {
+            clickCount++
+            textClicks.text = "Clicks: $clickCount"
+            Log.i("CountTAG", "count = $clickCount")
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    //
-    val clickCount = remember { mutableIntStateOf(0) }
-
-    Row(modifier = modifier) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Standard Image",
-            modifier = Modifier
-                .size(64.dp)
-                .clickable {
-                    clickCount.intValue++  // увеличиваем счётчик
-                }
-        )
-        Column(modifier = Modifier.padding(start = 16.dp)) {
-            Text(text = "Hello $name", fontSize = 24.sp)
-            Text(text = "Nice to meet you", fontSize = 16.sp)
-            Text(text = "Clicks: ${clickCount.intValue}", fontSize = 16.sp)
-            Log.i("CountTAG", "count = ${clickCount.intValue}")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Easy_APPTheme {
-        Greeting("Android")
-    }
-}
+//
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContent {
+//            Easy_APPTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    //
+//    val clickCount = remember { mutableIntStateOf(0) }
+//
+//    Row(modifier = modifier) {
+//        Image(
+//            painter = painterResource(id = R.drawable.ic_launcher_background),
+//            contentDescription = "Standard Image",
+//            modifier = Modifier
+//                .size(64.dp)
+//                .clickable {
+//                    clickCount.intValue++  // увеличиваем счётчик
+//                }
+//        )
+//        Column(modifier = Modifier.padding(start = 16.dp)) {
+//            Text(text = "Hello $name", fontSize = 24.sp)
+//            Text(text = "Nice to meet you", fontSize = 16.sp)
+//            Text(text = "Clicks: ${clickCount.intValue}", fontSize = 16.sp)
+//            Log.i("CountTAG", "count = ${clickCount.intValue}")
+//        }
+//    }
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    Easy_APPTheme {
+//        Greeting("Android")
+//    }
+//}
